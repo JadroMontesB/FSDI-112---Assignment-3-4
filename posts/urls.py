e2387957a011aa565f040a5ceb63b494c6859ca9
JadroMontesB <jadro.montes99@gmail.com>
2022-03-26 20:33:55 -0700

    First push

---
+++ posts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from posts.views import PostUpdateView 
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
+urlpatterns = [
+
+    path('', PostListView.as_view(), name='posts_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('new/', PostCreateView.as_view(), name='post_new'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+
+]
